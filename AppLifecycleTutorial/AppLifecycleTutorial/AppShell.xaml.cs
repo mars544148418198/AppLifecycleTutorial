21e125e1ef66b46f3be20d483389b95c0a43b34d
User <User@WIN-C9HQIV4VA1U>
2021-12-07 22:34:54 +0300

    Add project files.

---
+++ AppLifecycleTutorial/AppLifecycleTutorial/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using AppLifecycleTutorial.ViewModels;
+using AppLifecycleTutorial.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AppLifecycleTutorial
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
